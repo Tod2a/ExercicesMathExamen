04065cd9eaa53be9977b06d280d0b9cec0b867ad
Thomas <t.copin@yahoo.fr>
2024-05-26 15:59:50 +0200

    start of ex2.5 matrix

--- ExoPdf5/ex2.5.py
+++ ExoPdf5/ex2.5.py
@@ -21,9 +21,9 @@
 
 #----------------------------------------------------------------------#
 #                                                                      #
-#                           Exercice 2.4                               #
-#            Calcul de l'inverse par la méthode du pivot de            #                                                          
-#                           Gauss-Jordan                               #
+#                           Exercice 2.5                               #
+#                   Résolution d'un système d'équation                 #                                                          
+#                                                                      #
 #----------------------------------------------------------------------#
 
 #----------------------------------------------------------------------#
@@ -34,8 +34,8 @@
 #                        Que fait ce programme?                        #           
 #----------------------------------------------------------------------#
 #                                                                      #
-#             Ce programme calcule l'inverse d'une matrice             #
-#               à l'aide d'une fonction qui retournera l'inverse       #         
+#             Ce programme utilise les fonction de l'ex2.4             #
+#            pour résudre un système d'équation à l'aide de matrices   #         
 #                                                                      #
 #----------------------------------------------------------------------#
 
@@ -53,12 +53,44 @@ import keyboard                 #attention il faut installer la bibliothèque av
 # Encodage des fonctions 
 #-----------------------------------------------------------------------
 
-#ajustement des valeurs i et j afin que l'utilisateur puisse introduire la ligne 3 
-#il ne devra pas prendre en compte que informatiquement, l'index des tableau commence à 0
+def CreateMatrix(rows, cols):
+    grid = []
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            while True:
+                value = input(f"Introduisez la valeur numéro {j+1} de l'équation numéro {i+1}? ")
+                if value == '':
+                    print("\nAttention, vous avez introduit une valeur nulle!")
+                else:
+                    try:
+                        value_int = int(value)
+                        row.append(value_int)
+                        break
+                    except ValueError:
+                        print("\nAttention, vous devez entrer un nombre entier.")
+        grid.append(row)
+    return grid
+
+#réutilisation d'une fonction des exercices du Pdf1
+
+def BuildGridMultiplied(grid1, grid2):
+    grid = []
+    for i in range(len(grid1)):
+        row = []
+        for j in range(len(grid2[0])):
+            value = 0
+            for k in range(len(grid2)):
+                value += grid1[i][k] * grid2[k][j]
+            row.append(value)
+        grid.append(row)
+    return grid
+
+#réutilisation des fonctions de l'exercice 2.4 
 def substract_linesByAlpha(A, k, i, alpha):
-    for j in range(len(A[k-1])):
-        value = A[i-1][j]*alpha
-        A[k-1][j] = A[k-1][j]-value
+    for j in range(len(A[k])):
+        value = A[i][j]*alpha
+        A[k][j] = A[k][j]-value
 
 def print_matrice(A):
     for row in A:
@@ -93,15 +125,18 @@ def inverse(A):
     inverse_matrix = augmented_matrix[:, n:]
     return inverse_matrix.tolist()
 
+def solve(A, T):
+    #Utilisation de la fonction de numpy pour éviter les problèmes d'arrondis avec la fonction de base
+    #inv_A = np.linalg.inv(A)
+    #print_matrice(inv_A)
+    inv_test = inverse(A)
+    print_matrice(inv_test)
+    X = BuildGridMultiplied(inv_test, T)
+    return X
 #-----------------------------------------------------------------------
 # Définition / initialisation des variables               
 #-----------------------------------------------------------------------
 
-matrice = [
-    [1, 1, 2],
-    [1, 2, 1],
-    [2, 1, 1]
-]
 
 #-----------------------------------------------------------------------
 
@@ -114,29 +149,50 @@ matrice = [
 
 # Encodez votre programme ici!
 print("\n---------------------------------------------\n")
-print("\nCalcul de l'inverse d'une matrice\n")
+print("\nRésolution d'un système d'équiation à l'aide des matrices\n")
 print("\n---------------------------------------------\n")
+
+while True:
+    print("\nveuillez d'abord définir vos équations.")
+    col = int(input("\nCombien d'équations contient votre système?"))
+    lin = int(input("\nCombien de variables contiennent vos équations?"))
+    if col != lin:
+        print("\nIl faut que le nombre de variables soit égal au nombre d'équations, sinon la résolution du système n'est pas possible car la matrice ne sera pas carrée")
+    else:
+        break
+
+print("\n veuillez entrer les valeurs des coefficients")
+coef = CreateMatrix(lin, col)
+
+print("\nVeuillez introduire les valeurs des termes indépendants de vos équations:")
+t = CreateMatrix(lin, 1)
+
+print("\nVoici donc les Coefficients et les termes indépendants de vos équations:")
+print_matrice(coef)
+print_matrice(t)
+
+print("\nVoici les valeurs des variables de votre système, les valeurs se lisent de haut en bas")
 
 start = time.time()
 
-print("\n Matrice de base: ")
-print_matrice(matrice)
-
-print("\n inverse de cette matrice:")
-inv = inverse(matrice)
-print_matrice(inv)
-
-print("\nVérification avec Numpy:")
-test = np.array(matrice, dtype=float)
-inv = np.linalg.inv(test)
-print_matrice(inv)
+x = solve(coef, t)
 
 end = time.time()
 
+print_matrice(x)
+
+print("\nVérification avec Numpy:")
+
+a = np.array(coef)
+b = np.array(t)
+
+x = np.linalg.solve(a, b)
+
+print_matrice(x)        
 
 print("\n---------------------------------------------\n")
 
-print("\nvous avez calculé l'inverse de la matrice, l'opération vous a prit: ", end-start, " secondes")
+print("\nvous avez résolu votre système d'équation, l'opération vous a pris: ", end-start, " secondes")
 
 print("\nPoussez sur la touche q pour fermer cette fenêtre")
 
