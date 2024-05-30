61ebc5ad153c1fc1fc3f289f4ba3b7950e672391
Thomas <t.copin@yahoo.fr>
2024-05-30 15:03:40 +0200

    Exercice5 pdf6

--- ExoPdf6/Exercice5.py
+++ ExoPdf6/Exercice5.py
@@ -21,7 +21,7 @@
 
 #----------------------------------------------------------------------#
 #                                                                      #
-#                           Exercice 4                                 #
+#                           Exercice 5                                 #
 #                      Fonction de convolution                         #                                                          
 #                                                                      #
 #----------------------------------------------------------------------#
@@ -34,7 +34,7 @@
 #                        Que fait ce programme?                        #           
 #----------------------------------------------------------------------#
 #                                                                      #
-#   Création d'une fonction qui applique une filtre de flou linéaire   #
+#   Création d'une fonction qui applique une filtre de flou Gaussien   #
 #                                                                      #  
 #                                                                      #
 #----------------------------------------------------------------------#
@@ -50,22 +50,22 @@ import keyboard                 #attention il faut installer la bibliothèque av
 from scipy.ndimage import convolve
 from tkinter import *
 from PIL import Image, ImageTk
+from scipy.ndimage import gaussian_filter
 
 
 #-----------------------------------------------------------------------
 # Encodage des fonctions 
 #-----------------------------------------------------------------------
 
-def blur_filter(matrix, mask_size):
-    mask = np.ones((mask_size, mask_size)) / (mask_size * mask_size)
-    # Si l'image est en couleur, appliquer le filtre sur chaque canal séparément
+def gaussian_blur(matrix, sigma):
+    # Appliquer un flou gaussien à l'image
     if matrix.ndim == 3:
         blurred_matrix = np.zeros_like(matrix)
         for i in range(matrix.shape[2]):
-            blurred_matrix[:, :, i] = convolve(matrix[:, :, i], mask, mode='constant', cval=0.0)
+            blurred_matrix[:, :, i] = gaussian_filter(matrix[:, :, i], sigma=sigma)
         return blurred_matrix
     else:
-        return convolve(matrix, mask, mode='constant', cval=0.0)
+        return gaussian_filter(matrix, sigma=sigma)
 
 
 #-----------------------------------------------------------------------
@@ -86,30 +86,21 @@ image = np.asarray(img_in)
 #-----------------------------------------------------------------------
 
 # Encodez votre programme ici!
-print("\nFiltre de flou linéaire de Lenna avec un masque 3x3 et 10x10\n")
+print("\nFiltre de flou gaussien de Lenna avec un masque 3x3 et 10x10\n")
 print("\n---------------------------------------------\n")
 
 start = time.time()
 
-img_out1 = blur_filter(image, 3)      
+img_out1 = gaussian_blur(image, sigma=1)      
 
 end = time.time()
 
-print(f"\nle filtre 3x3 de lenna est réussit, l'opération vous a prit: {end-start} secondes")
-
-start2 = time.time()
-
-img_out2 = blur_filter(image, 10)
-
-end2 = time.time()
-
-print(f"\nle filtre 10x10 de lenna est réussit, l'opération vous a prit: {end2-start2} secondes")
+print(f"\nle filtre de flou gaussien de lenna est réussit, l'opération vous a prit: {end-start} secondes")
 
 print("\n---------------------------------------------\n")
 
 Image.fromarray(image).save("image_entree.png")
 Image.fromarray(img_out1).save("image_sortie.png")
-Image.fromarray(img_out2).save("image_sortie2.png")
 
 
 #-----------------------------------------------------------------------
@@ -135,9 +126,6 @@ photo = ImageTk.PhotoImage(image_in)
 image_out = Image.open("image_sortie.png")
 photo2 = ImageTk.PhotoImage(image_out)
 
-image_out2 = Image.open("image_sortie2.png")
-photo5 = ImageTk.PhotoImage(image_out2)
-
 canvas = Canvas(root,width=300,height=250,bg="silver")
 canvas.create_image(150,127, image=photo)
 canvas.pack()
@@ -147,10 +135,6 @@ champ_label_result2.pack()
 
 canvas = Canvas(root,width=300,height=250,bg="silver")
 canvas.create_image(150,127, image=photo2)
-canvas.pack()
-
-canvas = Canvas(root,width=300,height=250,bg="silver")
-canvas.create_image(150,127, image=photo5)
 canvas.pack()
 
 empty_line3 = Label(root, text="")
